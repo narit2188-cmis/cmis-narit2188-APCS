b6e9a9fcb80fa65d6a4770cf0e14d207c73c0d7e
Narit <narit2188@cmis.ac.th>
2019-08-09 11:43:39 +0700

    initial commit

---
+++ Unit1/PrintingStrings.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class PrintingStrings here.
+ *
+ * @author (Narit Trikasemsak)
+ * @version (0.1)
+ */
+public class PrintingStrings
+{
+   public static void main(String[] argv) {
+       String myString = new String("This is my string");
+       
+       System.out.println(myString);
+   
+    }
+}
