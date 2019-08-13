77b54220d690f2df2066ea37be1df958cfa9b09f
Narit <narit2188@cmis.ac.th>
2019-08-13 14:12:56 +0700

    with input

--- Unit1/PrintingStrings.java
+++ Unit1/PrintingStrings.java
@@ -5,12 +5,23 @@
  * @author (Narit Trikasemsak)
  * @version (0.1)
  */
+
+import java.util.Scanner; //import for input
 public class PrintingStrings
 {
    public static void main(String[] argv) {
+       Scanner input = new Scanner(System.in);
+       
        String myString = new String("This is my string");
        
+       String myName;
+       
+       myName = input.nextLine(); 
+       
        System.out.println(myString);
+       System.out.println(myName);
+       
+       System.out.format ("1: %s \n2: %s", myString, myName);
    
     }
 }
