6b9c2d675ef93e96a9dee9f08f293ad708e78391
Narit <narit2188@cmis.ac.th>
2019-08-15 09:55:10 +0700

    Datatypes

---
+++ Unit1/DatatypeVariableAssignment.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * @author Narit Trikasemsak
+ * @version 0.1
+ */
+public class DatatypeVariableAssignment
+{
+    public static void main (String[] argv) {
+    Scanner input = new Scanner (System.in);
+    System.out.println ("DatatypeVariableAssignment:");
+    
+    
+    }
+}
