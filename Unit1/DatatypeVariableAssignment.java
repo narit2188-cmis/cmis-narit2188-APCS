198018e0824ff42bc969895498f56cc77bb87ad8
Narit <narit2188@cmis.ac.th>
2019-08-16 11:42:49 +0700

    Primitive Datatypes, Variables, Assignment Statements, Expressions

--- Unit1/DatatypeVariableAssignment.java
+++ Unit1/DatatypeVariableAssignment.java
@@ -6,9 +6,53 @@ import java.util.Scanner;
 public class DatatypeVariableAssignment
 {
     public static void main (String[] argv) {
-    Scanner input = new Scanner (System.in);
-    System.out.println ("DatatypeVariableAssignment:");
-    
-    
+        Scanner input = new Scanner (System.in);
+        System.out.println ("DatatypeVariableAssignment:");
+
+        byte firstVar = 100;
+        short secondVar = 10000;
+        int thirdVar = 100000;
+        long fourthVar = 1000000;
+        float fifthVar = 1.2f;
+        double sixthVar = 1.2345;
+        boolean seventhVar = true; 
+        char eighthVar = '\u0000';
+
+        System.out.println(firstVar);
+        System.out.println(secondVar);
+        System.out.println(thirdVar);
+        System.out.println(fourthVar);
+        System.out.println(fifthVar);
+        System.out.println(sixthVar);
+        System.out.println(seventhVar);
+        System.out.println(eighthVar);
+
+        boolean apOne;
+        apOne = input.nextBoolean();
+
+        int apTwo;
+        apTwo = input.nextInt();
+
+        double apThree;
+        apThree = input.nextDouble();
+
+        System.out.format ("I am a boolean: %s\nI am an integer: %s\nI am a double: %s",
+            apOne, apTwo,apThree);
     }
-}
+
+    public static void typeTest() {
+        Scanner input = new Scanner (System.in);
+        System.out.println ("typeTest:");
+        
+        int intOne = input.nextInt();
+        int intTwo = input.nextInt();
+        int sumOne = intOne + intTwo;
+        int difOne = intOne - intTwo;
+        int prodOne = intOne * intTwo;
+        int quotOne = intOne / intTwo;
+        
+        System.out.format ("%s\n%s\n%s\n%s\n%s\n%s\n", 
+            intOne, intTwo, sumOne, difOne, prodOne, quotOne);
+            
+    }
+}   
