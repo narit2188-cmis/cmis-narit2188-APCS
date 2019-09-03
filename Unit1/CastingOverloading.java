a97ab299b6d4a7830f796b552c411181283f6897
Narit <narit2188@cmis.ac.th>
2019-09-03 11:11:50 +0700

    completed casting, etc

--- Unit1/CastingOverloading.java
+++ Unit1/CastingOverloading.java
@@ -8,38 +8,31 @@
 public class CastingOverloading
 {
     public static int oneorZero() {
-        double org = Math.random();
-        double result = org * 2 ;
-        return (int)result; 
+        return ((int)(Math.random() * 2)); 
     }
 
     public static int zeroTo99() {
-        double org = Math.random();
-        double result = org * 100;
-        return (int)result;
+
+        return ((int)(Math.random() * 100));
     }
     
     /*public static double zeroTo99() {
-        double org = Math.random();
-        double result = org * 100;
-        return result;
-    }
-    */
+        return ((Math.random() * 100));
+    }*/
+    
     public static int zeroToN(int n) {
-        double org = Math.random();
-        double result = org * (n+1);
-        return (int)result;
+        return ((int)(Math.random() * (n+1)));
     }
+    
     public static double zeroToN(double n) {
-        double org = Math.random();
-        double result = org * (n+1);
-        return result;
+        return (Math.random() * (n+1));
     }
     
     public static int minToMax(int min, int max) {
-        double org = Math.random();
-        double result =  org * (max +1);
-        return (int)result;
+        return ((int) ((Math.random()) * (max - min) + min));
     }
 
+    public static double minToMax(double min, double max) {
+        return (((Math.random()) * (max - min) + min));
+    }
 }
