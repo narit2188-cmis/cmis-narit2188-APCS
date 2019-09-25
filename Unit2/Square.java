3b3533a7a7e2962d5a1d2a2c05e03d03e8dd4d9f
Narit <narit2188@cmis.ac.th>
2019-09-25 11:55:57 +0700

    1

---
+++ Unit2/Square.java
@@ -0,0 +1,16 @@
+public class Square {
+    
+    private int sideLength = 5;
+    private String color = "red";
+    private int weight = 10;     
+    public Square (int side, String color, int weight){
+        this.sideLength = side;
+        this.color = color;
+        this.weight = 10;
+        return "Square legnth of " + 
+    }
+    
+    public String toString(){
+        return "I a string"; 
+    }
+}
---
+++ Unit2/SquareTester.java
@@ -0,0 +1,9 @@
+public class SquareTester {
+    
+    public static void main (String[]argv) {
+        Square s = new Square();
+        System.out.println (s);
+        Square a  = new Square();
+        System.out.println (a);
+    }
+}
