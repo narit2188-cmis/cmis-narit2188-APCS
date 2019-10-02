9882bace7df00e1f09965e515b5720b760e104ce
Narit <narit2188@cmis.ac.th>
2019-10-02 14:31:38 +0700

    2 left

--- Unit2/RegularPolygon.java
+++ Unit2/RegularPolygon.java
@@ -41,6 +41,9 @@ public class RegularPolygon
     public double getApothem(){
         return ((sideLength)/(2 * Math.tan(180/nSides)));
     }
+    public void setApothem(double newApothem){
+        sideLength = newApothem / (2 * Math.tan(180/nSides));
+    }
     /*public double getLengthOfLongestBisector () {
         
     }
@@ -52,5 +55,21 @@ public class RegularPolygon
         double newArea = pct * (0.5 * (nSides * sideLength) *((sideLength)/(2 * Math.tan(180/nSides))));
         sideLength = Math.sqrt(((2*newArea)/nSides) * 2 * Math.tan(180/nSides));
     }
-    
+    public int fitsIn(RegularPolygon other){
+        double result = this.getApothem() - other.getRadius(); 
+        return (int)result;
+    }
+    public void inscribe(RegularPolygon other){
+        double result =other.getApothem() - this.getRadius(); 
+        double result2 = this.getRadius() + result; 
+        this.setRadius(result2); 
+    }
+    public void circumscribe (RegularPolygon other) {
+        double result =this.getApothem() - other.getRadius(); 
+        double result2 = this.getApothem() - result; 
+        this.setApothem(result2); 
+    }
+    public int getInscribedNGon(int nSides) {
+        
+    }
 }
