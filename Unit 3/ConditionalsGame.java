1e45e61265c96d3cd4f628370dbd49daaed14780
Narit <narit2188@cmis.ac.th>
2019-11-21 12:34:19 +0700

    bleh

--- Unit 3/ConditionalsGame.java
+++ Unit 3/ConditionalsGame.java
@@ -11,53 +11,53 @@ public class ConditionalsGame
     }
     public static boolean f2(String length, String substring){
         int integer = length.length();
-        int wordl= substring.length();
+        int inta = substring.length();
         String stringo = substring.substring(0, integer);
-        String equals = substring.substring (wordl - integer);
+        String equals = substring.substring (inta - integer);
         if (length.equals(stringo) || length.equals(equals)){ return true;}
         else{return false;}}
-    public static boolean f3(int s1, int s2, int s3){
-        if ((s1 == s2) && (s1 == s3) && (s2 == s3) ) {
+    public static boolean f3(int firstrandom, int lengthOfString, int integer){
+        if ((firstrandom == lengthOfString) && (firstrandom == integer) && (lengthOfString == integer) ) {
             return true; 
         }
         else {return false;}
     }
-    public static String f4(String word, String c){
-        int position = word.indexOf(c); 
-        String one = word.substring(0,position);
-        String two = word.substring(position+1);
-        int lengthOne = one.length();
-        int lengthTwo = two.length();
-        if (lengthOne < lengthTwo){
-            return one;}
-        else if (lengthTwo < lengthOne){
-            return two;
+    public static String f4(String indexOf, String substring){
+        int mathpow = indexOf.indexOf(substring); 
+        String eleventyfive = indexOf.substring(0,mathpow);
+        String wooooooooooooooooooooooooooooooo = indexOf.substring(mathpow+1);
+        int string = eleventyfive.length();
+        int random = wooooooooooooooooooooooooooooooo.length();
+        if (string < random){
+            return eleventyfive;}
+        else if (random < string){
+            return wooooooooooooooooooooooooooooooo;
         }
-        else if (lengthOne == lengthTwo){
-            return (one + two);
+        else if (string == random){
+            return (eleventyfive + wooooooooooooooooooooooooooooooo);
         }
         else{return "";}
     }
-    public static boolean f5(int q, int x, int c){
+    public static boolean f5(int asdfasdfasfd, int gadsgadfg, int qwerqwererghw){
         int big = 0;
         int smallOne = 0;
         int smallTwo = 0; 
-        if ((q >= x) && (q >= c)){
-            big = q;
-            smallOne = x;
-            smallTwo = c; 
+        if ((asdfasdfasfd >= gadsgadfg) && (asdfasdfasfd >= qwerqwererghw)){
+            big = asdfasdfasfd;
+            smallOne = gadsgadfg;
+            smallTwo = qwerqwererghw; 
         }
         
-        else if ((x >= q) && (x >= c)){
-            big = x;
-            smallOne = q;
-            smallTwo = c; 
+        else if ((gadsgadfg >= asdfasdfasfd) && (gadsgadfg >= qwerqwererghw)){
+            big = gadsgadfg;
+            smallOne = asdfasdfasfd;
+            smallTwo = qwerqwererghw; 
         }
         
-        else if ((c >= x) && (c >= q)){
-            big = c;
-            smallOne = x;
-            smallTwo = q; 
+        else if ((qwerqwererghw >= gadsgadfg) && (qwerqwererghw >= asdfasdfasfd)){
+            big = qwerqwererghw;
+            smallOne = gadsgadfg;
+            smallTwo = asdfasdfasfd; 
         }
         double result = (Math.pow(smallOne, 2)) + (Math.pow(smallTwo, 2));
         System.out.println(result);
