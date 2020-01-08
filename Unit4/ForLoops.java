f48efb89fc128c325cda71fdbf14abe7b8a01ab7
narit <narit2188@cmis.ac.th>
2020-01-08 14:35:48 +0700

    for

---
+++ Unit4/ForLoops.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class ForLoops here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ForLoops
+{
+    public static String table(int rows, int columns, int operation){
+        if (operation == 0){
+            for (int x = 0; x<rows; x++){
+                for (int x = 0; x < columns, x++)
+        }
+    }
+}
