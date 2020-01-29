e03779f547a25b3236d3c69a36e6fda2a43f1b46
narit <narit2188@cmis.ac.th>
2020-01-29 11:45:14 +0700

    done

--- Unit5/Arrays.java
+++ Unit5/Arrays.java
@@ -7,7 +7,7 @@
  */
 public class Arrays
     
-{
+    {
     public static int max(int[] array){
         int result;
         int length;
@@ -140,13 +140,38 @@ public class Arrays
                 newArray[x-distance] = array[x]; 
             }
         }
-        return newArray;
-    /*public static int[] interleave(int[] array1, int[] array2){
-        return 2 arrays interleaved}
-    public static int[] maxValues(int[] array1, int[] array2){
+        return newArray;}
+    public static int[] interleave(int[] array1, int[] array2){
+        int length1 = array1.length;
+        int length2 = array2.length; 
+        int big = Math.max(length1,length2);
+        int small = Math.min(length1, length2);
+        int[] result = new int[big+small];
+        int y =0; 
+        for (int x=0; x<big;x++){
+            if ((x)>=(length1-1)){
+                result[y] = array1[x];
+                System.out.print(result[y]);
+                System.out.print(y);
+                y++; 
+                for (int z = x;z<big;z++){
+                    System.out.print(x);
+
+                    result[y]=array2[z];
+                }
+                return result; 
+            }
+            else{
+            result[y]= array1[x];
+            result[y+1]= array2[x]; 
+            y+=2;}
+        }
+        return result; 
+    }
+    /*public static int[] maxValues(int[] array1, int[] array2){
         return new array with max value from each index}
     public static boolean allUnique(int[] array){
         return true if all items in array are unique}
     public static int[] uniqueValues(int[] array){
         return new array containing unique values from array} */
-    }}
+    }
