e12d6f7cca7bcee40d90abf672130d46637a34e1
narit <narit2188@cmis.ac.th>
2020-02-27 09:47:27 +0700

    arraylists pt1

--- Unit7/ArrayLists.java
+++ Unit7/ArrayLists.java
@@ -2,14 +2,26 @@ import java.util.*;
 public class ArrayLists
 {
     public static void main(String[] argv){
-        int random = (int) (Math.random() * 10); 
         List<Integer> original = new ArrayList<Integer>(); 
-        for (int x = 0; x < random; x ++){
+        for (int x = 0; x < 10; x ++){
             original.add((int) (Math.random() * 10)); 
         }
+        List<Double> originalDub = new ArrayList<Double>(); 
+        for (int x = 0; x < 10; x ++){
+            Double add = Math.random() * 10; 
+            originalDub.add(add); 
+        }
+        List<String> originalS = new ArrayList<String>(); 
+        originalS.add("fish");
+        originalS.add("cat");
+        originalS.add("dog");
         System.out.println("original " + original); 
         System.out.println("getOdds " + getOdds(original)); 
-        System.out.pritnln("avg " + insertAvgs(original)); 
+        System.out.println("original double " + originalDub); 
+        insertAvgs(originalDub); 
+        System.out.println("averages inserted " + originalDub); 
+        System.out.println("original string " + originalS);
+        System.out.println("Chopped " + chopper(originalS)); 
     }
     
     public static List<Integer> getOdds(List<Integer> al){
@@ -28,7 +40,7 @@ public class ArrayLists
     public static void insertAvgs(List<Double> al){
         /*insert the average between each pair of integers in the arraylist*/
         int size = al.size() + (al.size()-1); 
-        for (int x = 0; x < al.size(); x+=2){
+        for (int x = 0; x < al.size()-1; x+=2){
             Double avg = (al.get(x) + al.get(x+1) )/ 2; 
             al.add(x+1, avg); 
         }
@@ -39,7 +51,25 @@ public class ArrayLists
          * front half of each word from words combined with the
          * back half of the next word.
          */
-            return null;
+        List<String> chopped = new ArrayList<String>();
+            for (int x = 0; x < words.size(); x++){
+              String current = words.get(x);
+              if (x+1 == words.size()){
+                  String next = words.get(0); 
+                  int currentSize = current.length()/2; 
+                  int nextSize = next.length()/2; 
+                  String insert = current.substring(0,currentSize) + next.substring(nextSize); 
+                  chopped.add(x,insert); 
+                }
+              else{
+                  String next = words.get(x+1); 
+                  int currentSize = current.length()/2; 
+                  int nextSize = next.length()/2;
+                  String insert = current.substring(0,currentSize) + next.substring(nextSize); 
+                  chopped.add(x,insert); 
+                }
+            }
+            return chopped;
     }
 
 }
