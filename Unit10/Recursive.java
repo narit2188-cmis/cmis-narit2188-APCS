24883b829e1a202489a3713ad7668d13e98a5aa8
nart <narit2188@cmis.ac.th>
2020-03-11 11:56:24 +0700

    recursives

---
+++ Unit10/Recursive.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class Recursive here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Recursive
+{
+    public static void main (String argv[]){
+        System.out.println(multiply(5,5));
+        System.out.println(power(5,5)); 
+        System.out.println(factorial(5)); 
+        System.out.println(divide(100,5)); 
+    }
+    public static int multiply (int n, int x){
+        if (x == 0){
+            return 0;  
+        }
+        else {
+           return (multiply(n,x-1)+n);
+        }
+    }
+    public static int power (int n, int x){
+        if (x == 0){
+            return 1;  
+        }
+        else {
+           return (power(n,x-1)*n);
+        }
+    }
+    public static int factorial (int n){
+        if (n == 0){
+            return 1;  
+        }
+        else {
+           return (factorial(n-1)*n);
+        }
+    }
+    public static int divide (int n, int x){
+        if (n == 0){
+            return 0;  
+        }
+        else {
+           return (divide(n-x,x) - x);
+        }
+    }
+}
